03499314c53fdb1b5c5ece3b3fe9cdcb39b05ca3
THARUS <2025tharuni@gmail.com>
2026-09-04 10:30:40 +0530

    Time: 5 ms (83.81%), Space: 121.6 MB (68.47%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
@@ -1,43 +1,19 @@
 class Solution {
-    public boolean uniformArray(int[] arr) {
-        if(checkAllEven(arr)) return true;
-    else if(checkAllOdd(arr)) return true;
-    for(int i = 0; i < arr.length; i++){
-        if(arr[i] % 2 == 0){
-            int diff = 0;
-            for(int j = 0; j < i; j++){
-                if(arr[i]-arr[j] >= 1){
-                    diff++;
-                    break;
-                }
+    public boolean uniformArray(int[] nums1) {
+        int minEven = Integer.MAX_VALUE;
+        int minOdd = Integer.MAX_VALUE;
+        for(int i : nums1){
+            if(i % 2 == 0){
+                minEven = Math.min(i,minEven);
             }
-            if(diff < 1 && i < arr.length-1){
-                for(int j = i+1; j < arr.length; j++){
-                    if(arr[i]-arr[j] >= 1){
-                        diff++;
-                        break;
-                    }
-                }
+            else{
+                minOdd = Math.min(i,minOdd);
             }
-            if(diff < 1) return false;
         }
+        if(minEven == Integer.MAX_VALUE || minOdd == Integer.MAX_VALUE){
+            return true;
+        }
+        return minOdd < minEven;
+        
     }
-    return true;
-    }
-
-
-public boolean checkAllEven(int[] arr){
-    for(int i : arr){
-        if(i % 2 != 0)return false;
-    }
-    return true;
-}
-
-public boolean checkAllOdd(int[] arr){
-    for(int i : arr){
-        if(i % 2 == 0) return false;
-    }
-    return true;
-}
-
 }
